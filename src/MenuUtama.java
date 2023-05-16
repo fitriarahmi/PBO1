b567acfdb745df8395f705d7560537cde8ee796b
fitriarahmi <fitriarahmi25@gmail.com>
2023-05-15 22:16:33 -0700

    first commit

---
+++ src/MenuUtama.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class MenuUtama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        konsumen obj = new konsumen();
+        obj.setIDKONS("01");
+        obj.setNAMAUSER("Rahmi");
+        obj.setPASS("1234");
+        obj.setEMAILUSER("RAHMI123@GMAIL.COM");
+        obj.setNAMAPRODUK("bAJU");
+        obj.setKOTA("Banjarmaasin");
+        System.out.println("ID : "+obj.getIDKONS() +"Nama uSER "+obj.getNAMAUSER() +"\n PASS "+obj.getPASS() +"\n EMAIL " +obj.getEMAILUSER()+ "\n Nama PRODUK "+obj.getNAMAPRODUK()+ "\n KOta "+obj.getKOTA());
+    }
+    
+}
---
+++ src/konsumen.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class konsumen {
+    private String idkons, namauser, pass, emailuser, namaproduk, kota;
+    public konsumen(){}
+    
+    public void setIDKONS(String idkons){
+        this.idkons=idkons;
+    }
+    
+    public String getIDKONS(){
+        return this.idkons;
+    }
+
+    public void setNAMAUSER(String namauser){
+        this.namauser=namauser;
+    }
+    
+    public String getNAMAUSER(){
+        return this.namauser;
+    }
+
+     public void setPASS(String pass){
+        this.pass=pass;
+    }
+    
+    public String getPASS(){
+        return this.pass;
+    }
+    public void setEMAILUSER(String emailuser){
+        this.emailuser=emailuser;
+    }
+    
+    public String getEMAILUSER(){
+        return this.emailuser;
+    }
+
+    public void setNAMAPRODUK(String namaproduk){
+        this.namaproduk=namaproduk;
+    }
+    
+    public String getNAMAPRODUK(){
+        return this.namaproduk;
+    }
+
+     public void setKOTA(String kota){
+        this.kota=kota;
+    }
+    
+    public String getKOTA(){
+        return this.kota;
+    }
+}
